be3dacf2176c111fc3b5decbe21d12fbb22cce63
Gurvinder Singh <gurvinder.singh@uninett.no>
2018-11-06 15:50:33 +0100

    add basic tests for endpoint checking

---
+++ test_app.py
@@ -0,0 +1,19 @@
+import pytest
+
+from molten import testing
+from app import app
+
+client = testing.TestClient(app)
+
+def test_index_returns_200():
+    resp = client.get(app.reverse_uri("health"))
+    assert resp.status_code == 200
+    assert resp.json()['version'] == '0.0.1'
+
+def test_index_not_allowed_post():
+    resp = client.post(app.reverse_uri("health"))
+    assert resp.status_code == 404
+
+def test_index_not_allowed_get():
+    resp = client.get(app.reverse_uri("predict"))
+    assert resp.status_code == 404
